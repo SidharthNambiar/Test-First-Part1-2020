b2661e6b3202c35176a61ba0601f2247cb795a1c
Sidharth Nambiar <snambiar01@gmail.com>
2020-05-15 18:27:56 -0400

    03 complete

--- 03-factory-functions/calculator.js
+++ 03-factory-functions/calculator.js
@@ -1,1 +1,47 @@
 /* eslint-disable no-unused-vars */
+
+function createCalculator() {
+
+    return {
+        total: 0,
+
+        offset: 0,
+
+        value: function () {
+            return this.total;
+        },
+
+        add: function (num) {
+            this.total += num;
+        },
+
+        subtract: function (num) {
+            this.total -= num;
+        },
+
+        clear: function () {
+            this.total = this.offset;
+        }
+
+
+    }
+}
+
+function addSquareMethod(arrOfInstances) {
+    for (let i = 0; i < arrOfInstances.length; i++) {
+        arrOfInstances[i]['square'] = function () {
+            return this.total * this.total;
+        }
+    }
+
+    return arrOfInstances;
+}
+
+function createHumanCalculator() {
+
+    const newObj = Object.create(createCalculator());
+    newObj.total = -10;
+    newObj.offset = -10;   
+
+    return newObj;
+}
